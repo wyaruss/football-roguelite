4a95eec93d6690be03b24b21bf319094596965ab
Wyatt Russell <wyaruss@gmail.com>
2024-12-04 13:08:27 -0700

    Set up Player abstract base class

--- Player.py
+++ Player.py
@@ -1,3 +1,20 @@
-class Player:
-    def __init__(self, name):
+from abc import ABC, abstractmethod
+import random
+
+class Player(ABC):
+    @classmethod
+    def generate_stat(cls, mean: int, sd: int) -> int: 
+        return random.gauss(mean, sd) 
+    
+    @classmethod
+    @abstractmethod
+    def stat_metadata(cls) -> dict: 
+        pass
+
+    def __init__(self, name: str):
         self.name = name
+
+        self.stats = {
+            stat: self.generate_stat(values["mean"], values["std"])
+            for stat, values in self.stat_metadata().items()
+        }
